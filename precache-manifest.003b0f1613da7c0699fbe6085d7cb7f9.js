ecebdfc2b19696c75a993f448104ac9b291ab447
Cathy Hsieh <cchsieh@ucdavis.edu>
2020-12-09 19:11:08 -0800

    Updates

---
+++ precache-manifest.003b0f1613da7c0699fbe6085d7cb7f9.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda0b1925e70a1f899c7840cc3e66140",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dc45e62edf6a4e9e0cc6",
+    "url": "/Portfolio/static/css/main.cc398399.chunk.css"
+  },
+  {
+    "revision": "c031f237c15c84ffd66d",
+    "url": "/Portfolio/static/js/2.4a9da110.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.4a9da110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc45e62edf6a4e9e0cc6",
+    "url": "/Portfolio/static/js/main.c94bdeed.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  },
+  {
+    "revision": "42cd4cdc6facffd9fac9b691c1ce0f51",
+    "url": "/Portfolio/static/media/Hsieh_References.42cd4cdc.pdf"
+  },
+  {
+    "revision": "65d01c67f79e891d471f1bd891a799f2",
+    "url": "/Portfolio/static/media/Hsieh_Resume.65d01c67.pdf"
+  },
+  {
+    "revision": "3fd010aaefc5ad5d418a1fe10dff3853",
+    "url": "/Portfolio/static/media/arrow.3fd010aa.svg"
+  },
+  {
+    "revision": "0560ba4d7c48bb6c0ac57d449c690ad8",
+    "url": "/Portfolio/static/media/bg1.0560ba4d.svg"
+  },
+  {
+    "revision": "dcf483e2a0b0e5b1f66c4093b3f33ecb",
+    "url": "/Portfolio/static/media/cathy.dcf483e2.png"
+  },
+  {
+    "revision": "f76da2e3ddc65899d5a2c89d4db60497",
+    "url": "/Portfolio/static/media/ceed.f76da2e3.png"
+  },
+  {
+    "revision": "66e22057d54aeb7b91c2b9a1c27abf8b",
+    "url": "/Portfolio/static/media/ceed1.66e22057.png"
+  },
+  {
+    "revision": "ea2881defe7e486df911b92aaff54541",
+    "url": "/Portfolio/static/media/chilly.ea2881de.png"
+  },
+  {
+    "revision": "cd04d4c7124a4e7c672afb466740b48c",
+    "url": "/Portfolio/static/media/cni1.cd04d4c7.png"
+  },
+  {
+    "revision": "64ca72af91e01e904fe87b460fd6f806",
+    "url": "/Portfolio/static/media/cni2.64ca72af.png"
+  },
+  {
+    "revision": "0733ab60706c346ff507de2736bf9dd3",
+    "url": "/Portfolio/static/media/cold.0733ab60.png"
+  },
+  {
+    "revision": "7d44d93f989b7a94293cd72305390c40",
+    "url": "/Portfolio/static/media/email.7d44d93f.png"
+  },
+  {
+    "revision": "34119f98c3ce7550d146fc81aee9bf49",
+    "url": "/Portfolio/static/media/github.34119f98.svg"
+  },
+  {
+    "revision": "d5bbab3801bb9a4f9ebccf684587e3b1",
+    "url": "/Portfolio/static/media/hot.d5bbab38.png"
+  },
+  {
+    "revision": "054c565f49e0ab074bd52cbed3ad3a54",
+    "url": "/Portfolio/static/media/jot.054c565f.png"
+  },
+  {
+    "revision": "7472c1dcc9a0a2c611b4c34c2a0ae6b8",
+    "url": "/Portfolio/static/media/me.7472c1dc.JPG"
+  },
+  {
+    "revision": "08877c6cc2150d3e34326cb634789c05",
+    "url": "/Portfolio/static/media/me1.08877c6c.jpg"
+  },
+  {
+    "revision": "5edab78d7050b321a5477c45c3f58013",
+    "url": "/Portfolio/static/media/o1.5edab78d.png"
+  },
+  {
+    "revision": "845296f57d5832d3fe6ca4f6641e99b4",
+    "url": "/Portfolio/static/media/o2.845296f5.png"
+  },
+  {
+    "revision": "fc733e2a91e29875d06313c493379b4d",
+    "url": "/Portfolio/static/media/o3.fc733e2a.png"
+  },
+  {
+    "revision": "a9a6fe3cb5c8fbf1247824c2089e2241",
+    "url": "/Portfolio/static/media/opDoc.a9a6fe3c.pdf"
+  },
+  {
+    "revision": "1e47277e719d7534bc44a1b0f863592a",
+    "url": "/Portfolio/static/media/perfect.1e47277e.png"
+  },
+  {
+    "revision": "4c03e668f792a9dd7e69d9691d0bd68a",
+    "url": "/Portfolio/static/media/person.4c03e668.png"
+  },
+  {
+    "revision": "319d45b4b1ea350bc5acfd154b582dd1",
+    "url": "/Portfolio/static/media/phone.319d45b4.png"
+  },
+  {
+    "revision": "f6b5a8014c8cdc2b960e22f9860bf9bf",
+    "url": "/Portfolio/static/media/toast.f6b5a801.png"
+  },
+  {
+    "revision": "c3e127db49b50955db99989662d81830",
+    "url": "/Portfolio/static/media/toast1.c3e127db.png"
+  },
+  {
+    "revision": "c4acc88680de207f69e7ac8624f17fdb",
+    "url": "/Portfolio/static/media/toast2.c4acc886.png"
+  },
+  {
+    "revision": "ed119e625f3526725ecf84497e0d5142",
+    "url": "/Portfolio/static/media/ucd.ed119e62.png"
+  },
+  {
+    "revision": "c58c8b0e41423676b1305ad90af1364c",
+    "url": "/Portfolio/static/media/warm.c58c8b0e.png"
+  }
+]);
