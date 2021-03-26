0d5a3d45d0a113bd67e53e76d9e4cf1c0ea825f2
Cathy Hsieh <cchsieh@ucdavis.edu>
2021-03-25 22:32:59 -0700

    updated work tab description and added cni images

--- src/Components/Menu/intro.js
+++ src/Components/Menu/intro.js
@@ -11,11 +11,11 @@ class intro extends Component {
                         ABOUT ME
                     </div>
                     <p className="p1" >
-                    I’m a recent graduate at the University of California - Davis, with a Bachelor in Computer Science and Engineering.
+                        I’m a recent graduate at the University of California - Davis, with a Bachelor in Computer Science and Engineering.
                         <br/><br/>
                         I enjoy coding and love creativity. So with the combination of the two, I love frontend and UX/UI!
                         <br/><br/>
-                        I had a year of experience as a UI/Frontend Web Developer Intern at UC Davis - Facility Energy & Engineering, from July 2019 to October 2020. I love my work and do enjoy working. I want to extend my skills and knowledge by looking for a similar career.
+                        I had a year of experience as a UI/Frontend Web Developer Intern at UC Davis from July 2019 to October 2020. I love my work and do enjoy working. I want to extend my skills and knowledge by looking for a similar career.
                         <br/><br/>
                         I also love playing games, including video and board games. Also, sports like badminton and tennis.
                     </p>
--- src/Components/Menu/work.js
+++ src/Components/Menu/work.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 import Jot from "../../images/jot.png"
 import Ceed from "../../images/ceed.png"
+import OurJourney from "../../images/journey.png"
+import EmissionMap from "../../images/emissionMap.png"
+import arrow from "../../images/arrow.svg"
+
+const TOTAL_PAGE = 2
 
 class work extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            page: 0
+        }
+    }
 
-    render() {
-        return (
-            <div className="grid tab work">
-                 <div className="text-wrapper work">
-                    <div className="header">
-                        WORK EXPERIENCE
-                    </div>
-                    <div className="sub-header">
-                        • UI / Frontend Developer Intern
-                        <span className="date">Jul. 2019 ~ Oct. 2020</span>
-                    </div>
+    clickPage = direction => {
+        let page = this.state.page
+        if(direction == "left") {
+            page--
+        }
+        else if(direction == "right") {
+            page++
+        }
+        if(page < 0) page = 0
+        if(page > TOTAL_PAGE - 1) page = TOTAL_PAGE - 1
+
+        this.setState({ page })
+    }
+
+    description(page) {
+        if(page === 0) {
+            return (
+                <p className="p1" >
+                    In my team, I primarily responsible for building and maintaining the frontend development. All of our projects are 100% custom-built for UC Davis.
                     <br/>
-                    <p className="p1" >
+                    <br/>
                     I supported and developed several web-based applications and features for existing UC website and projects, 
                     such as the {" "}
                     <a href="https://ceed.ucdavis.edu/" target="_blank">
@@ -28,17 +47,78 @@ class work extends Component {
                     <span className="sub-text"><i>(Both JoT and CNI are still under development)</i></span>
                     <br/>
                     <br/>
-                        During my web developer internship at UC Davis, I strengthened my abilities and developed numerous skills such as HTML, CSS/SCSS, React JSX, GraphQL, and many other areas. Our team worked in an agile development environment, which has sharpened my communication and teamwork skills. 
-                    </p>
+                    During the internship, my colleague, who focused on backend development, and I were determined to take on new challenges for ourselves. Fortunately, we were able to work on a new project (Carbon Neutrality Initiative Dashboard). My primary responsibility is to implement and design the frontend components. 
+                </p>
+            )
+        }
+        else {
+            return (
+                <p className="p1" >
+                    During those times, I collaborated with a backend colleague, product developer, and data teams weekly to ensure the project is going in the right direction. Critical teamwork skills I learned are to communicate well, actively listen, and be responsible and honest. 
+                    <br/>
+                    <br/>
+                    In the end, we successfully developed the project from the ground up. Also, we were able to pass this project to another department to build on more features. It was the most accomplished project I've worked on during my internship experience.
+                    <br/>
+                    <br/>
+                    My internship experience has given me the skills to adjust to the changing dynamics while meeting scheduled milestones and deliverables.
+                </p>
+            )
+        }
+    }
+
+    projectImage(page) {
+        if(page === 0) {
+            return (
+                <div>
+                    <div className="img-wrap">
+                        <img className="screenshot" src={Jot} alt="Jot icon"/>
+                        <i>TherMOOstat</i>
+                    </div>
+                    <div className="img-wrap ceed">
+                        <img className="screenshot" src={Ceed} alt="CEED icon"/>
+                        <i>CEED</i>
+                    </div>
                 </div>
-                <div className="img-wrap">
-                    <img className="screenshot" src={Jot} alt="Jot icon"/>
-                    <i>TherMOOstat</i>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <div className="img-wrap">
+                        <img className="screenshot" src={OurJourney} alt="Jot icon"/>
+                        <i>CNI - OUR JOURNEY</i>
+                    </div>
+                    <div className="img-wrap ceed">
+                        <img className="screenshot" src={EmissionMap} alt="CEED icon"/>
+                        <i>CNI - EMISSIONS MAP</i>
+                    </div>
                 </div>
-                <div className="img-wrap ceed">
-                    <img className="screenshot" src={Ceed} alt="CEED icon"/>
-                    <i>CEED</i>
+            )
+        }
+    }
+
+    render() {
+        let page = this.state.page
+        return (
+            <div className="grid tab work">
+                 <div className="text-wrapper work">
+                    <div className="header">
+                        WORK EXPERIENCE
+                    </div>
+                    <div className="sub-header">
+                        • UI / Frontend Developer Intern
+                        <span className="date">Jul. 2019 ~ Oct. 2020</span>
+                    </div>
+                    {this.description(page)}
+                    <div className="buttons">
+                        <img className="arrow-button left" src={arrow} alt="Arrow icon" onClick={() => this.clickPage("left")}/>
+                        <span className="pages">
+                            {page + 1} of {TOTAL_PAGE}
+                        </span>
+                        <img className="arrow-button" src={arrow} alt="Arrow icon" onClick={() => this.clickPage("right")}/>
+                    </div>
                 </div>
+                {this.projectImage(page)}
             </div>
         )
     }
